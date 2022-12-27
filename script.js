30b6fb2b8c5ba407bc687b1b431bd4b432f10079
dsuvankar <98757134+dsuvankar@users.noreply.github.com>
2022-12-27 20:26:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let textbox = document.getElementById("textbox");
+
+textbox.addEventListener("input", function () {
+  var text = this.value;
+  //   console.log(text);
+  let char = text.length;
+
+  document.getElementById("char").innerHTML = char;
+
+  text = text.trim();
+  let words = text.split(" ");
+
+  let cleanList = words.filter(function (element) {
+    return element != "";
+  });
+
+  document.getElementById("word").innerHTML = cleanList.length;
+});
